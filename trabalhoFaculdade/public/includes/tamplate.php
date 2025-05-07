fa51810fd9155e3f89af56a64fb5e992425c80bd
JhonatanBrugnari <brugnari.jhonatan@gmail.com>
2025-05-07 20:12:59 -0300

    Add files via upload

---
+++ trabalhoFaculdade/public/includes/tamplate.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Castoro+Titling&display=swap" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-SgOJa3DmI69IUzQ2PVdRZhwQ+dy64/BUtbMJw1MZ8t5HZApcHrRKUc4W0kG879m7" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/js/bootstrap.bundle.min.js" integrity="sha384-k6d4wzSIapyDyv1kpU366/PK5hCdSbCRGRCMv+eplOQJWyd1fbcAu9OCUj5zNLiq" crossorigin="anonymous"></script>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pagina Inicial</title>
+    <link rel="stylesheet" href="../public/css/style.css">
+</head>
+
+<body>
+    <main>
+
+      
+
+
+        <ul class="nav justify-content-end">
+            <li class="nav-item_ext.links">
+            <a href="*"><i class="fa-brands fa-instagram"></i></a><a href="*"><i class="fa-brands fa-whatsapp"></i></a>
+            </li>
+
+            <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="index.php">Pagina inicial</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="contato.php">Contato</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="cardapio.php">Cardapio</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="reservas.php">Reservas</a>
+            </li>
+
+        </ul>
+    </main>
+
+
+
+    <footer style="text-align: center; color: gold; margin-top: 50px;">Famiglia Piconni & Ramalho&copy;</footer>
+
+</body>
+
+</html>
